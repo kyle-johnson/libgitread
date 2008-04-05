f0b97388d490ffef03c6d544ffefe5213c83fb0e
Kyle Johnson <osmaker@gmail.com>
2008-04-05 16:28:20 -0700

    Pygitutil.c updated to add a PackIdx object for better control over load/unload_idx.

--- gitutils/pygitutil.c
+++ gitutils/pygitutil.c
@@ -1,9 +1,128 @@
 #include <Python.h>
+#include <structmember.h> // also a Python include
+
 #include "libgitread.h"
+
+typedef struct {
+    PyObject_HEAD
+    struct idx *idx;
+    PyObject *location;
+} PackIdxObject;
+
+static void PackIdx_dealloc(PackIdxObject *self)
+{
+    // close and free up everything as needed...
+    if(self->idx)
+        unload_idx(self->idx);
+    Py_XDECREF(self->location);
+        
+    // have to do this
+    self->ob_type->tp_free((PyObject*)self);
+}
+
+static PyObject *PackIdx_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
+{
+    PackIdxObject *self;
+    
+    self = (PackIdxObject *)type->tp_alloc(type, 0);
+    if(self != NULL) {
+        // setup values
+        self->idx = NULL;
+        self->location = NULL;
+    }
+    
+    return (PyObject *)self;
+}
+
+static int PackIdx_init(PackIdxObject *self, PyObject *args, PyObject *kwds)
+{
+    PyObject *location;
+    
+    if(!PyArg_ParseTuple(args, "S", &location))
+        return -1; // error
+    
+    // quick error check, since it is possible to call __init__ more than once (which we don't support)
+    if(self->idx != NULL || self->location != NULL) {
+        PyErr_SetString(PyExc_Exception, "This object has already been intialized once.");
+        return -1;
+    }
+    
+    // load up the idx
+    self->idx = load_idx(PyString_AsString(location));
+    
+    if(!self->idx) {
+        PyErr_SetString(PyExc_Exception, "Failed to load the idx file.");
+        return -1;
+    }
+    
+    // save the location for easy access from Python
+    self->location = location;
+    Py_INCREF(location);
+    
+    return 0; // success
+}
+
+/*static void PackIdx_close(PackIdxObject *self)
+{
+    
+}*/
+
+static PyMemberDef PackIdx_members[] = {
+    {"location", T_OBJECT, offsetof(PackIdxObject, location), READONLY, "location of the idx file"},
+    {NULL}
+};
+
+/*static PyMethodDef PackIdx_methods[] = {
+    {"close", (PyCFunction)PackIdx_close, METH_NOARGS,
+        "Closes the idx file properly. This method is called if you simply delete the object."},
+    {NULL}
+};*/
+
+static PyTypeObject PackIdxType = {
+    PyObject_HEAD_INIT(NULL)
+    0,                         /*ob_size*/
+    "gitutil.PackIdx",         /*tp_name*/
+    sizeof(PackIdxObject),     /*tp_basicsize*/
+    0,                         /*tp_itemsize*/
+    (destructor)PackIdx_dealloc, /*tp_dealloc*/
+    0,                         /*tp_print*/
+    0,                         /*tp_getattr*/
+    0,                         /*tp_setattr*/
+    0,                         /*tp_compare*/
+    0,                         /*tp_repr*/
+    0,                         /*tp_as_number*/
+    0,                         /*tp_as_sequence*/
+    0,                         /*tp_as_mapping*/
+    0,                         /*tp_hash */
+    0,                         /*tp_call*/
+    0,                         /*tp_str*/
+    0,                         /*tp_getattro*/
+    0,                         /*tp_setattro*/
+    0,                         /*tp_as_buffer*/
+    Py_TPFLAGS_DEFAULT,        /*tp_flags*/
+    "PackIdx objects",         /* tp_doc */
+    0,		                   /* tp_traverse */
+    0,		                   /* tp_clear */
+    0,		                   /* tp_richcompare */
+    0,		                   /* tp_weaklistoffset */
+    0,		                   /* tp_iter */
+    0,		                   /* tp_iternext */
+    0,//PackIdx_methods,           /* tp_methods */
+    PackIdx_members,           /* tp_members */
+    0,                         /* tp_getset */
+    0,                         /* tp_base */
+    0,                         /* tp_dict */
+    0,                         /* tp_descr_get */
+    0,                         /* tp_descr_set */
+    0,                         /* tp_dictoffset */
+    (initproc)PackIdx_init,    /* tp_init */
+    0,                         /* tp_alloc */
+    PackIdx_new,               /* tp_new */
+};
 
 static PyObject *raw_tree_to_pyobject(struct git_object *g_obj)
 {
-    unsigned char *source_internal_buffer, *src_buff, *end;
+    unsigned char /**source_internal_buffer,*/ *src_buff, *end;
     PyObject *pylist;
     PyObject *pytuple;
     PyObject *pyfilename;
@@ -155,28 +274,22 @@ static PyObject *gu_loose_get_object(PyObject *self, PyObject *args)
 
 static PyObject *gu_pack_idx_read(PyObject *self, PyObject *args)
 {
-    char *location;
+    PackIdxObject *idx;
     char *sha1;
     struct idx_entry *entry;
-    struct idx *idx;
     struct sha1 hash;
     PyObject *ret;
     
-    if(!PyArg_ParseTuple(args, "ss", &location, &sha1))
+    if(!PyArg_ParseTuple(args, "Os", &idx, &sha1))
         return NULL;
 
     if(str_sha1_to_sha1_obj(sha1, &hash) != 0)
         return NULL;
     
-    if(!(idx = load_idx(location)))
-        return NULL;
-
-    entry = pack_idx_read(idx, &hash);
-    
-    unload_idx(idx);
+    entry = pack_idx_read(idx->idx, &hash);
     
     if(entry != NULL) {
-        ret = Py_BuildValue("is", entry->offset, entry->sha1);
+        ret = Py_BuildValue("is", entry->offset, sha1_to_hex(entry->sha1));
         free(entry);
         return ret;
     } else {
@@ -193,5 +306,18 @@ static PyMethodDef git_util_methods[] = {
 
 PyMODINIT_FUNC initgitutil(void)
 {
-    (void) Py_InitModule("gitutil", git_util_methods);
+    PyObject *m;
+    
+    // initial type setup
+    if(PyType_Ready(&PackIdxType) < 0)
+        return;
+    
+    m = Py_InitModule("gitutil", git_util_methods);
+    
+    if(m == NULL)
+        return;
+    
+    // now actually add the type
+    Py_INCREF(&PackIdxType);
+    PyModule_AddObject(m, "PackIdx", (PyObject *)&PackIdxType);
 }
